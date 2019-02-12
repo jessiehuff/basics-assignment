1771c0c611b59c467d83ebe18cbd4f238e98a074
Jessie Huff <huffjd1@gmail.com>
2019-02-12 15:40:01 -0500

    Practiced creating new angular components, using modules, and nesting components

---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+    selector: 'app-warning', 
+    templateUrl: './warning.component.html',
+    styles: [`
+        .warning {
+            color: red; 
+        }
+    `]
+})
+
+export class WarningAlert {
+
+}
